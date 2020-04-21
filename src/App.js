9d3524026e893d5f1fe5338ad878ec0f34615bc9
codementee <codementee@outlook.com>
2020-04-21 20:54:44 +0500

    update

--- src/App.js
+++ src/App.js
@@ -18,26 +18,38 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imgUrl: ''
+      imgUrl: '', 
+      box: {}
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImg');
+    const width = image.width;
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width), 
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFace = (box) => {
+    console.log(box)
+    this.setState({box: box});
   }
 
   onInputChange = (event)=> {
     this.setState({input: event.target.value});
   }
 
-
-
   onSubmit = () => {
     this.setState({imgUrl: this.state.input})
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-    function(response) {
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-    },
-    function(err) {
-      // there was an error
-    }
-  );
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(response => this.displayFace(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err))
   }
 
   render() {
@@ -49,7 +61,7 @@ class App extends Component {
         </Header>
         <Rank />
         <InputLink onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
-        <Result imgUrl={this.state.imgUrl}/>
+        <Result box={this.state.box} imgUrl={this.state.imgUrl}/>
       </div>
     );
   }
--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Result.css';
 
-const Result = ({ imgUrl }) => {
+const Result = ({ imgUrl, box }) => {
     return (
-        <div>
-            <img alt="" src={imgUrl} />
+        <div className="ma center">
+            <div className="absolute mt2">
+               <img id="inputImg" alt="" src={imgUrl} width='500px' height='auto'/>
+             <div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+            </div>
         </div>
     )
 }
